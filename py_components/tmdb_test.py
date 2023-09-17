1a95a30d36818c602bc24992a0aec55cbe2ca5d6
Robert Vari <mail.robertvari@gmail.com>
2023-09-17 16:10:56 +0200

    update

--- py_components/tmdb_test.py
+++ py_components/tmdb_test.py
@@ -11,8 +11,13 @@ def get_popular_movies(page):
         title = i.get("title")
         release_date = i.get("release_date")
         vote_average = i.get("vote_average") * 10
-        poster_path = None
+        poster_path = f"{POSTER_ROOT_PATH}{i.get('poster_path')}"
 
-    pass
+        print("-"*50)
+        print(f"Title: {title}")
+        print(f"Date: {release_date}")
+        print(f"Popularity: {vote_average}")
+        print(f"Poster: {poster_path}")
+        print("-"*50)
 
-get_popular_movies(2)
+get_popular_movies(1)
