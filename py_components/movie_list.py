97f9b94f08d1632878ed7f8a15259837b9126416
Robert Vari <mail.robertvari@gmail.com>
2023-09-30 15:01:53 +0200

    update

--- py_components/movie_list.py
+++ py_components/movie_list.py
@@ -2,6 +2,8 @@ from typing import Optional
 from PySide6.QtCore import QAbstractListModel, QModelIndex, Qt, QObject, QRunnable, Signal, QThreadPool, Property
 from py_components.resources import get_image_from_url
 import tmdbsimple as tmdb
+import time
+
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
 
@@ -23,6 +25,8 @@ class MovieList(QAbstractListModel):
         self._fetch()
     
     def _fetch(self):
+        self.download_progress_changed.emit()
+
         self._movie_list_worker.signals.task_finished.connect(self._insert_movie)
         self._job_pool.start(self._movie_list_worker)
 
@@ -30,6 +34,8 @@ class MovieList(QAbstractListModel):
         self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
         self._movies.append(movie_data)
         self.endInsertRows()
+
+        self.download_progress_changed.emit()
 
     def rowCount(self, parent=QModelIndex) -> int:
         return len(self._movies)
@@ -69,6 +75,7 @@ class MovieListWorker(QRunnable):
     def _fetch(self):
         popular_movies = self.movies.popular(page=1)["results"]
         for i in popular_movies:
+            time.sleep(0.5)
             title = i.get("title")
             release_date = i.get("release_date")
             vote_average = i.get("vote_average") * 10
@@ -79,4 +86,4 @@ class MovieListWorker(QRunnable):
                 "release_date": release_date,
                 "vote_average": vote_average,
                 "poster_path": poster_path
-            })
+            })            
