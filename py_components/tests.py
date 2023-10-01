47686ec369cac32d9d6721c29ec20ccb543ef926
Robert Vari <mail.robertvari@gmail.com>
2023-10-01 17:07:02 +0200

    update

--- py_components/tests.py
+++ py_components/tests.py
@@ -1,9 +1,12 @@
 import tmdbsimple as tmdb
-from py_components.resources import get_image_from_url
+import resources
 tmdb.API_KEY = '83cbec0139273280b9a3f8ebc9e35ca9'
 tmdb.REQUESTS_TIMEOUT = 5
+
+BACKDROP_URL = f"https://image.tmdb.org/t/p/w1920_and_h800_multi_faces"
 
 movie = tmdb.Movies(278)
 movie_data = movie.info()
 backdrop_url = movie_data.get("backdrop_path")
 
+print(f"{BACKDROP_URL}{backdrop_url}")
